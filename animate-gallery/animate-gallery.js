e165c4fb86d3f6b832883cdbf584d655782877d6
mcyee <mingcee@skysketches.com>
2015-10-13 21:14:37 -0400

    fixed indentation and css include

--- animate-gallery/animate-gallery.js
+++ animate-gallery/animate-gallery.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(function() {
 	var currentPosition = 0;
 	var slideWidth = 540;
 	var slides = $('.slide');
@@ -9,12 +9,12 @@ $(document).ready(function() {
 
 	// Wrap all .slides with #slideInner div
 	slides
-	.wrapAll('<div id="slideInner"></div>')
-	// Float left to display horizontally, readjust .slides width
-	.css ({
-		'float' : 'left',
-		'width' : slideWidth
-	});
+		.wrapAll('<div id="slideInner"></div>')
+		// Float left to display horizontally, readjust .slides width
+		.css ({
+			'float' : 'left',
+			'width' : slideWidth
+		});
 	
 	// Set #slideInner width equal to total width of all slides
 	$('#slideInner').css('width', slideWidth * numberOfSlides);
@@ -31,34 +31,34 @@ $(document).ready(function() {
 	$('.control')
 		.bind('click', function() {
 			// Determine new position
-				currentPosition = ($(this).attr('id')=='rightControl')
-			? currentPosition+1 : currentPosition-1;
+			currentPosition = ($(this).attr('id')=='rightControl')
+				? currentPosition+1 : currentPosition-1;
 
-				// Hide / show controls
-				manageControls(currentPosition);
-				// Move slideInner using margin-left
-				$('#slideInner').animate({
-					'marginLeft' : slideWidth*(-currentPosition)
-				});
+			// Hide / show controls
+			manageControls(currentPosition);
+			// Move slideInner using margin-left
+			$('#slideInner').animate({
+				'marginLeft' : slideWidth*(-currentPosition)
 			});
+		});
 
-		// manageControls: Hides and shows controls depending on currentPosition
-		function manageControls(position) {
-			// Hide left arrow if position is first slide
-			if(position==0){
-	$('#leftControl').hide()
-			}
-			else { 
-	$('#leftControl').show() 
-			}
-			
-			// Hide right arrow if position is last slide
-			if(position==numberOfSlides-1) {
-	$('#rightControl').hide()
-}
-
-			else {
-	$('#rightControl').show() 
-}
+	// manageControls: Hides and shows controls depending on currentPosition
+	function manageControls(position) {
+		// Hide left arrow if position is first slide
+		if(position==0){
+			$('#leftControl').hide()
 		}
-	});
+		else { 
+			$('#leftControl').show() 
+		}
+		
+		// Hide right arrow if position is last slide
+		if(position==numberOfSlides-1) {
+			$('#rightControl').hide()
+		}
+
+		else {
+			$('#rightControl').show() 
+		}
+	}
+});
