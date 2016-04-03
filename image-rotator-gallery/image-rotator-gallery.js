6bb4fa8cabd97fe7ce9017bea05b436c554eb235
mcyee <mingcee@skysketches.com>
2016-04-02 23:40:57 -0400

    moved things into function

--- image-rotator-gallery/image-rotator-gallery.js
+++ image-rotator-gallery/image-rotator-gallery.js
@@ -2,55 +2,60 @@
 // Reference: http://michaelsoriano.com/how-to-create-a-jquery-image-rotator/
 
 $(document).ready(function() {
+	function switchContent() {
+		// swap the HTML of the block, then pull the block container back up
+		$("#main_image .block")
+			.html(imgDesc)
+			.animate({
+					opacity: 0.85,
+					marginBottom: "0"
+				},
+				250
+			);
+		// Switch the main image (URL + alt tag)
+		$("#main_image img").attr({
+			src: imgTitle,
+			alt: imgAlt
+		});
+	};
+
+	function rotateImage() {
+		var imgAlt        = $(this).find('img').attr("alt");
+		var imgTitle      = $(this).find('a').attr("href");
+		var imgDesc       = $(this).find('.block').html();
+		var imgDescHeight = $("#main_image").find('block').height();
+
+		// Don't click through - prevents repetitive animations on active/selected list-item
+		if ($(this).is(".active")) {
+			return false;
+		}
+		else {
+			// Animate the description
+			// Pull block down (negative bottom margin of its own height)
+			$("#main_image .block").animate({
+					opacity: 0,
+					marginBottom: -imgDescHeight
+				},
+				250,
+				switchContent
+			);
+		}
+
+		//Show active list-item
+		$(".image_thumb ul li").removeClass('active');
+		$(this).addClass('active');
+
+		return false;
+	};
+
 	$("#main_image .desc").show();
 	$("#main_image .block").animate({ opacity:0.85 }, 1);
-	//Add the active class (highlights the very first list item by default)
+	// highlight the very first list item by default
 	$(".image_thumb ul li:first").addClass('active');
 
 	// click thumbnail
 	$(".image_thumb ul li")
-		.click(function() {
-			var imgAlt        = $(this).find('img').attr("alt");
-			var imgTitle      = $(this).find('a').attr("href");
-			var imgDesc       = $(this).find('.block').html();
-			var imgDescHeight = $("#main_image").find('block').height();
-
-			// Don't click through - prevents repetitive animations on active/selected list-item
-			if ($(this).is(".active")) {
-				return false;
-			}
-			else {
-				// Animate the description
-				// Pull block down (negative bottom margin of its own height)
-				$("#main_image .block").animate({
-						opacity: 0,
-						marginBottom: -imgDescHeight
-					},
-					250,
-					function() {
-						//swap the HTML of the block, then pull the block container back up and set opacity
-						$("#main_image .block")
-							.html(imgDesc)
-							.animate({
-									opacity: 0.85,
-									marginBottom: "0"
-								},
-								250
-							);
-						// Switch the main image (URL + alt tag)
-						$("#main_image img").attr({
-							src: imgTitle,
-							alt: imgAlt
-						});
-				});
-			}
-
-			//Show active list-item
-			$(".image_thumb ul li").removeClass('active');
-			$(this).addClass('active');
-
-			return false;
-		})
+		.click(rotateImage)
 		.hover(function() { $(this).addClass('hover'); },
 		       function() { $(this).removeClass('hover'); }
 		);
